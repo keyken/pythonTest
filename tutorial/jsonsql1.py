46ef652ebf3c23e7128d74912938eaaa635bc9ba
keyken <593219230@qq.com>
2018-01-24 17:03:02 +0800

    init commit

--- tutorial/jsonsql1.py
+++ tutorial/jsonsql1.py
@@ -2,7 +2,7 @@ import MySQLdb
 import json
 
 data = []
-conn = MySQLdb.connect(host='localhost', port=3306, user='root', passwd='keykid', db='zhaoping')
+conn = MySQLdb.connect(host='localhost', port=3306, user='root', passwd='123456', db='zhaoping')
 cur = conn.cursor()
 with open('dmoz_data_utf8.json') as f:
     for line in f:
