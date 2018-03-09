473857e260de3a58d75d044587add4a5b174a291
keyken <593219230@qq.com>
2018-03-09 13:20:15 +0800

    TEST

--- tutorial/jsonsql1.py
+++ tutorial/jsonsql1.py
@@ -1,13 +1,15 @@
+# -*- coding: utf-8 -*-
+
 import MySQLdb
 import json
 
 data = []
-conn = MySQLdb.connect(host='localhost', port=3306, user='root', passwd='123456', db='zhaoping')
+conn = MySQLdb.connect(host='localhost', port=3306, user='root', passwd='123456', db='zhaoping1')
 cur = conn.cursor()
 with open('dmoz_data_utf8.json') as f:
     for line in f:
-        tsql = "insert into jsondata(data) values('{json}')"
-        print(line)
+        tsql = "insert into jsondataTest2(info) values('{json}')"
+        print(MySQLdb.escape_string(line))
         sql = tsql.format(json=MySQLdb.escape_string(line))
         cur.execute(sql)
         conn.commit()
